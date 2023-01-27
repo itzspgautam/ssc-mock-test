23173d6f4d3c41fb503651b3ff240d22bcf808bb
unknown <itzspgautam@gmail.com>
2023-01-28 03:39:08 +0530

    Commit for SSCEXAMS

--- Frontend/src/Constants/Apis.js
+++ Frontend/src/Constants/Apis.js
@@ -1,6 +1,7 @@
 export const Api = {
-  CLOUDINARY: "https://api.cloudinary.com/v1_1/dvydxtrom/upload",
-  CLOUDINARY_PRESET: "sscexam2023",
-  //"onlineexamdesk",
-  //   CLOUDINARY: "https://api.cloudinary.com/v1_1/dtpspzd66/upload",
+  // CLOUDINARY: "https://api.cloudinary.com/v1_1/dvydxtrom/upload",
+  // CLOUDINARY_PRESET: "sscexam2023",
+
+  CLOUDINARY: "https://api.cloudinary.com/v1_1/dtpspzd66/upload",
+  CLOUDINARY_PRESET: "onlineexamdesk",
 };
